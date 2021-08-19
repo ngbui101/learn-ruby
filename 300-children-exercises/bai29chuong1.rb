1b83cc8c4b5ef6456191568f3308d139234ac45d
ngbui101 <88971570+ngbui101@users.noreply.github.com>
2021-08-20 01:35:04 +0200

    Update bai29chuong1.rb

--- 300-children-exercises/bai29chuong1.rb
+++ 300-children-exercises/bai29chuong1.rb
@@ -3,8 +3,9 @@ Tim uoc so le lon nhat cua so nguyen duong n nhap tu ban phim
 =end
 
 n = gets.chomp.to_i
-max = 0
-for i in 1..n
-	max = i if max < i && n%i == 0 && i%2 != 0 
+for i in n.downto(1) 
+	if n%i == 0 && i%2 != 0
+	break
+	end
 end
-puts max
+puts i
