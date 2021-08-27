3de950adc2217e3d70a0e8b7327d64c8b9fd5a4d
ngbui101 <buinguyenlam@gmail.com>
2021-08-27 23:50:54 +0200

    update array exercises

---
+++ 300-children-exercises/array-exercises/ex1.rb
@@ -0,0 +1,25 @@
+=begin
+Tinh trung binh cong so le o cac vi tri chan
+=end
+a = 10.times.map {rand(-10..10)}
+h = 0
+j = 0
+for i in 0..(a.length-1)
+	if i % 2 == 0 && a[i] % 2 != 0
+		#puts a[i]
+		h += a[i] 
+		j += 1
+	end 
+end
+puts "Arithmetic Mean ist #{h/j}" 
+
+
+
+
+#puts a.inspect  
+#=begin
+#t = a.values_at(*a.each_index.select {|i| i.even?} & a.select(&:odd?)
+
+#puts t
+ 
+#=end
