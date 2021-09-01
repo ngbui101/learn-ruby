3eeb2232e22e23033ede1a2060f354adab5de21d
ngbui101 <buinguyenlam@gmail.com>
2021-09-01 20:49:54 +0200

    update n

--- 300-children-exercises/array-exercises/ex6.rb
+++ 300-children-exercises/array-exercises/ex6.rb
@@ -3,7 +3,6 @@ Thay the cac gia tri am co trong mang bang 0
 =end
 arr = 10.times.map{rand(-10..10)}  
 negativ = []
-arr.each_with_index{|a, i| a < 0 ? negativ.push(i):nil}  	  
-negativ.each{|a| arr[a] = 0} 
-puts arr.inspect
+arr.each{|a| negativ.push(a < 0 ? 0 : a)} 
+puts negativ.inspect
 
--- 300-children-exercises/array-exercises/ex7.rb
+++ 300-children-exercises/array-exercises/ex7.rb
@@ -2,13 +2,6 @@
 xoa cac phan tu am co trong mang 
 =end
 arr = 10.times.map{rand(-10..10)} 
-=begin
-#puts a.map{|x| x < 0 ? '' : x}.inspect 
-a.delete_if {|x| x < 0}.inspect
-#a.drop_while {|x| x < 0}.inspect 
-=end
 negativ = []
-arr.each_with_index{|a, i| a < 0 ? negativ.push(i) : nil} 
-negativ.each{|a| arr[a] = nil}
-arr.compact!
-puts arr.inspect
+arr.each{|a| negativ.push(a < 0 ? nil : a )} 
+puts negativ.compact.inspect
