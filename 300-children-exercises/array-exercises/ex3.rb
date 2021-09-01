a442a485091e17ed0e3ac595d361ba8e8bda4d13
ngbui101 <buinguyenlam@gmail.com>
2021-09-01 20:17:44 +0200

    update ex3.rb

--- 300-children-exercises/array-exercises/ex3.rb
+++ 300-children-exercises/array-exercises/ex3.rb
@@ -3,15 +3,9 @@ tim vi tri cac so nho nhat trong mang
 =end
 arr = 10.times.map{rand(-10..10)}
 min = arr[0] 
-count = 0
-arr.each_with_index do |a, i|
-	if min >= a 
-		min = a
-		count = i	
-	end	
-end
-index = [arr.find_index(min) + 1, count +1]
-puts "Vi tri cac so nho nhat trong mang la #{index.uniq}"
-puts arr.inspect
-
+min_index = []
+arr.each {|a| min > a ? a = min : nil} 
+arr.each_with_index {|a, i| a == min ? min_index.push(i+1) : nil}
+puts "Vi tri cac so nho nhat trong mang la #{min_index.join(",")}"
+  
 
